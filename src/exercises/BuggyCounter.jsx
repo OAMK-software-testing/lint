1edb7aaa7e6369d276fa60fcf9b3799be9099282
Jouni Juntunen <jouni.juntunen@oamk.fi>
2026-08-31 13:18:12 +0300

    Merge master fixes and complete BuggyCounter.jsx solution

--- src/exercises/BuggyCounter.jsx
+++ src/exercises/BuggyCounter.jsx
@@ -7,27 +7,20 @@ function BuggyCounter() {
     document.title = `Count is ${count}`
   }, [count])
 
-  // Bug: declared but never used
-  const unusedValue = 42
-
   function increment() {
-    // Bug: `cout` is not defined anywhere (typo for `count`)
-    setCount(cout + 1)
+    setCount(count + 1)
   }
 
   function reset() {
     setCount(0)
-    return
-    // Bug: this line can never run
-    console.log('resetting')
   }
 
   function describe(value) {
     switch (value) {
-      // Bug: lexical declaration in a case block without braces
-      case 1:
+      case 1: {
         const label = 'one'
         return label
+      }
       default:
         return 'unknown'
     }
@@ -36,7 +29,7 @@ function BuggyCounter() {
   return (
     <div className="buggy-counter">
       <p>Count is {count}</p>
-      <img src="/counter-icon.svg" />
+      <img src="/counter-icon.svg" alt="Counter icon" />
       <button type="button" onClick={increment}>Increment</button>
       <button type="button" onClick={reset}>Reset</button>
       <p>{describe(count)}</p>
